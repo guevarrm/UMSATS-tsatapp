fe0920844020a5bea1aa704e711abc506b7aff0e
Michael-Mac <michaelguevarra@DESKTOP-2T91MKD.hitronhub.home>
2020-02-09 19:31:40 -0600

    final oauth

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 // keys.js - figure out what set of cred to return
 if (process.env.NODE_ENV == 'production') {
   //we are in prod
-  module.exports = requires('./prod');
+  module.exports = require('./prod');
 } else {
   // we are in dev
   module.exports = require('./dev');
-  
+
 }
