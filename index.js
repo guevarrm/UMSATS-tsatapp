01797e29bb56fe8c000986a9606423087a1ec67e
guevarrm <michael.guevarra97@gmail.com>
2020-02-17 15:30:31 -0600

    changes line 1

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Michael
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
+
 
 require("./models/User");
 require("./services/passport");
