b3ed916092a55f981ec7ac5bb79414e7c1f788c9
santaselucid <abraha13@myumanitoba.ca>
2020-02-17 14:54:52 -0600

    'test'

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport')
-const keys = require('./config/keys');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
 
-require('./models/User');
-require('./services/passport');
+require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
@@ -16,22 +16,22 @@ app.use(
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
-)
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
-// sanjay
+// lalalalallaa
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
- // localhost:5000 SERVER
- // http://localhost:5000/auth/google/
+// localhost:5000 SERVER
+// http://localhost:5000/auth/google/
 
- // localhost:3000 CLIENT
- // http://localhost:3000/auth/google/ -> proxy reroutes to ...:5000
+// localhost:3000 CLIENT
+// http://localhost:3000/auth/google/ -> proxy reroutes to ...:5000
 
 //update heroku:
 // git add .
