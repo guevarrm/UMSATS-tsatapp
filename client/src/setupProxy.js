87cd01a7d6820f42865aaf97bf70a2ca3882a0f4
guevarrm <michael.guevarra97@gmail.com>
2020-03-10 13:59:45 -0500

    proxy fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,6 +2,8 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy(["/api", "/auth/google", "/sat"], { target: "http://localhost:5000" })
+    proxy(["/api", "/auth/google", "/sat/login", "/sat/getInfo"], {
+      target: "http://localhost:5000"
+    })
   );
 };
